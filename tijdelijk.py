39f7ddb6f86fa2e217f5c506f874e953e5d822e3
Kamal Shirzad <kamalshirzad@outlook.com>
2025-02-05 14:40:41 +0100

    aanpassing prijs

--- tijdelijk.py
+++ tijdelijk.py
@@ -4,8 +4,8 @@ prijzen = {
 'chocolade' : 5
 }
 aanbieding = prijzen['aardbei'] * 0.8
-reclame_tekst = f'Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts €{aanbieding}'
-reclame_tekst2 = reclame_tekst[:61]
+reclame_tekst = f'Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}'
+reclame_tekst2 = reclame_tekst[:63]
 reclame_tekst3 = reclame_tekst2.upper()
 reclame_tekst4 = reclame_tekst3.split()
 for el in reclame_tekst4:
