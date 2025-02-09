39f6418360c82e32c91c27c8a4e2ad19315b1427
Kamal Shirzad <kamalshirzad@outlook.com>
2025-02-09 19:32:34 +0100

    presentatie en boekhouding

---
+++ presentatie.py
@@ -0,0 +1,11 @@
+def presenteer(inkomsten, totaal):
+    for k,v in inkomsten.items():
+        print(f" {k} : {v} euro")
+    
+    print('=' * 26)
+
+    print(f"totaal : {totaal} euro")
+
+
+    
+
